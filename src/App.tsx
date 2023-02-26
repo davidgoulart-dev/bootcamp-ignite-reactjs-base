499a5abf51365ae641b8c13c34047d243534efc0
David Goulart <119085211+davidgoulart-dev@users.noreply.github.com>
2023-02-26 22:27:42 +0000

    att
    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { RepositoryList } from './components/RepositoryList';
 
 function App() {
   return (
     <>
+      <RepositoryList/>
     
     </>
   );
