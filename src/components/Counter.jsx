84d44f3e4ffabc41a1e0b9c31516a0612304e2bf
David Goulart <119085211+davidgoulart-dev@users.noreply.github.com>
2023-02-26 23:08:23 +0000

    Estado no React
    Estudando sobre State no React

---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+export function Counter() {
+    let counter = 0;
+    
+    function increment() {
+        console.log("incrementing");
+
+    }
+    
+    return (
+        <div>
+            <h2>{counter}</h2>
+            <button type="button" onClick={increment}>
+                Increment
+            </button>
+        </div>
+    )
+}
